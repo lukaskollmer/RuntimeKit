bb0846dbd63ef1f81b5677a083b63876fff950bb
Lukas Kollmer <lukas.kollmer@gmail.com>
2017-03-30 17:12:55 +0200

    Initial Commit

---
+++ RuntimeKit/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RuntimeKit
+//
+//  Created by Lukas Kollmer on 30/03/2017.
+//  Copyright © 2017 Lukas Kollmer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
