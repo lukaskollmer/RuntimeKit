2343cdac653633413f754f716fb4dc93ca3a758e
Lukas Kollmer <lukas.kollmer@gmail.com>
2017-03-31 07:19:02 +0200

    Added a new Runtime.swift file with a struct that can be extended for general runtime-related things

---
+++ RuntimeKit/Sources/Runtime.swift
@@ -0,0 +1,11 @@
+//
+//  Runtime.swift
+//  RuntimeKit
+//
+//  Created by Lukas Kollmer on 31.03.17.
+//  Copyright © 2017 Lukas Kollmer. All rights reserved.
+//
+
+import Foundation
+
+struct Runtime { }
