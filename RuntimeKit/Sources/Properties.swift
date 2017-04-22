55a3e1d9579e2990d785cc3aefdecf2b3bc320d1
Lukas Kollmer <lukas.kollmer@gmail.com>
2017-04-22 12:00:36 +0200

    `ObjCPropertyDescription` now exposes more information about a property
    Things like:
    - type encoding
    - type (the actual class if it’s id)
    - copy/retain/nonatomis/weak/dynamic/etc

--- RuntimeKit/Sources/Properties.swift
+++ RuntimeKit/Sources/Properties.swift
@@ -9,8 +9,11 @@
 import Foundation
 import ObjectiveC
 
+// Reference: https://developer.apple.com/library/content/documentation/Cocoa/Conceptual/ObjCRuntimeGuide/Articles/ocrtPropertyIntrospection.html
+
+
 /// Struct describing an Objective-C property
-public struct ObjCPropertyDescription {
+public struct ObjCPropertyDescription: CustomStringConvertible {
     /// The property's name
     public var name: String {
         return String(cString: property_getName(property))
@@ -18,8 +21,106 @@ public struct ObjCPropertyDescription {
     
     public let property: objc_property_t
     
+    public private(set) var typeEncoding: ObjCTypeEncoding = .unknown
+    
+    public private(set) var type: NSObject.Type?
+    
+    /// The property is read-only (readonly).
+    public private(set) var isReadOnly = false
+    
+    /// The property is a copy of the value last assigned (copy).
+    public private(set) var isCopy = false
+    
+    /// The property is a reference to the value last assigned (retain).
+    public private(set) var isRetain = false
+    
+    /// The property is non-atomic (nonatomic).
+    public private(set) var isNonatomic = false
+    
+    /// The property defines a custom getter selector name. The name follows the G (for example, GcustomGetter,).
+    public private(set) var customGetter: String?
+    
+    /// The property defines a custom setter selector name. The name follows the S (for example, ScustomSetter:,).
+    public private(set) var customSetter: String?
+    
+    /// The property is dynamic (@dynamic).
+    public private(set) var isDynamic = false
+    
+    /// The property is a weak reference (__weak).
+    public private(set) var isWeak = false
+    
+    /// The property is eligible for garbage collection.
+    public private(set) var isEligibleForGarbageCollection = false
+    
+    /// Specifies the type using old-style encoding.
+    public private(set) var oldStyleTypeEncoding: String? = nil
+    
+    
     init(_ property: objc_property_t) {
         self.property = property
+        
+        ///print(String(cString: property_getAttributes(property)))
+        
+        var count: UInt32 = 0
+        let attributeList = property_copyAttributeList(property, &count)
+        
+        for i in 0..<count {
+            let attribute = attributeList.unsafelyUnwrapped[Int(i)]
+            
+            let name = String(cString: attribute.name)
+            let value = String(cString: attribute.value)
+            
+            switch name {
+            case "T":
+                /**
+                 TODO: This should detect:
+                 - [x] objects (id) and their objc class
+                 - [ ] structs and maybe even create an object describing the struct
+                 - [ ] other type encoding codes like 'r' for const, etc (https://developer.apple.com/library/content/documentation/Cocoa/Conceptual/ObjCRuntimeGuide/Articles/ocrtTypeEncodings.html)
+                 */
+                var value = value
+                if value.hasPrefix("@\"") && value.hasSuffix("\"") {
+                    value = value
+                        .replacingOccurrences(of: "@\"", with: "")
+                        .replacingOccurrences(of: "\"", with: "")
+                }
+                if (Runtime.classExists(value)) {
+                    self.typeEncoding = .object
+                    self.type = Runtime.getClass(value)
+                } else {
+                    self.typeEncoding = ObjCTypeEncoding(rawValue: value) ?? .unknown
+                }
+            case "R":
+                self.isReadOnly = true
+            case "C":
+                self.isCopy = true
+            case "&":
+                self.isRetain = true
+            case "N":
+                self.isNonatomic = true
+            case "G":
+                self.customGetter = value
+            case "S":
+                self.customSetter = value
+            case "D":
+                self.isDynamic = true
+            case "W":
+                self.isWeak = true
+            case "P":
+                self.isEligibleForGarbageCollection = true
+            case "t":
+                self.oldStyleTypeEncoding = value // TODO this is not tested
+            default: break
+            }
+        }
+        
+        free(attributeList)
+
+    }
+    
+    public var description: String {
+        let name = String(cString: property_getName(property))
+        return "[objc_property name: \(name)]"
     }
 }
 
