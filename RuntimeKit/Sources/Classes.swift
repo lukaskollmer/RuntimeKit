7c0859d430ecb8842d05df0c7c8ed8f6f5634324
Lukas Kollmer <lukas.kollmer@gmail.com>
2017-05-15 20:57:30 +0200

    Added `Runtime.destroy(class:)`

--- RuntimeKit/Sources/Classes.swift
+++ RuntimeKit/Sources/Classes.swift
@@ -7,55 +7,39 @@
 //
 
 import Foundation
-import ObjectiveC
-
-/// Struct describing an Objective-C class
-public struct ObjCClassDescription {
-    /// The classes name
-    public let name: String
-    
-    /// The actual class object (`AnyClass`)
-    public let `class`: AnyClass
-    
-    /// All protocols implemented by the class
-    private let protocols: [Protocol]
-    
-    /// Create a new `ObjCClassDescription` from an `AnyClass` object
-    ///
-    /// - Parameter `class`: A class
-    public init(_ `class`: AnyClass) {
-        self.name = String(cString: class_getName(`class`))
-        self.class = `class`
-        self.protocols = [] // TODO
-    }
-}
-
 
 public extension Runtime {
-    /// All classes registered with the Objective-C runtime
-    public static var allClasses: [ObjCClassDescription] {
-        var count: UInt32 = 0
-        let classList = objc_copyClassList(&count)
-        
-        var allClasses = [ObjCClassDescription]()
-        
-        for i in 0..<count {
-            guard let `class` = classList.unsafelyUnwrapped[Int(i)] else { continue }
-            allClasses.append(ObjCClassDescription(`class`))
+    /// Register a new class with the Objective-C runtime
+    ///
+    /// - Parameters:
+    ///   - name: The name of the new class
+    ///   - superclass: The superclass. Defaults to `NSObject`
+    ///   - protocols: All protocols the class should adopt
+    /// - Returns: The new class
+    /// - Throws: `RuntimeKitError.classnameAlreadyTaken` if the classname isn't available anymore, `RuntimeKitError.unableToCreateClass` if there was an error registering the new class
+    public static func createClass(_ name: String, superclass: AnyClass = NSObject.self, protocols: [Protocol] = []) throws  -> NSObject.Type {
+        guard !Runtime.classExists(name) else {
+            throw RuntimeKitError.classnameAlreadyTaken
         }
         
-        return allClasses
+        guard let newClass = objc_allocateClassPair(superclass, name.cString(using: .utf8)!, 0) else {
+            throw RuntimeKitError.unableToCreateClass
+        }
+        
+        objc_registerClassPair(newClass)
+        
+        protocols.forEach {
+            class_addProtocol(newClass, $0)
+        }
+        
+        guard let castedClass = newClass as? NSObject.Type else {
+            fatalError("wtf")
+        }
+        
+        return castedClass
     }
     
-    /// Check whether a class with the given name exists
-    ///
-    /// - Parameter name: A class name
-    /// - Returns: `true` if the class exists, otherwise `false`
-    public static func classExists(_ name: String) -> Bool {
-        return objc_getClass(name.cString(using: .utf8)) != nil
-    }
-    
-    public static func getClass(_ name: String) -> NSObject.Type? {
-        return objc_getClass(name.cString(using: .utf8)) as? NSObject.Type
+    public static func destroy(class cls: NSObject.Type) {
+        objc_disposeClassPair(cls)
     }
 }
---
+++ RuntimeKit/Sources/GetClasses.swift
@@ -0,0 +1,61 @@
+//
+//  GetClasses.swift
+//  RuntimeKit
+//
+//  Created by Lukas Kollmer on 31.03.17.
+//  Copyright © 2017 Lukas Kollmer. All rights reserved.
+//
+
+import Foundation
+import ObjectiveC
+
+/// Struct describing an Objective-C class
+public struct ObjCClassDescription {
+    /// The classes name
+    public let name: String
+    
+    /// The actual class object (`AnyClass`)
+    public let `class`: AnyClass
+    
+    /// All protocols implemented by the class
+    private let protocols: [Protocol]
+    
+    /// Create a new `ObjCClassDescription` from an `AnyClass` object
+    ///
+    /// - Parameter `class`: A class
+    public init(_ `class`: AnyClass) {
+        self.name = String(cString: class_getName(`class`))
+        self.class = `class`
+        self.protocols = [] // TODO
+    }
+}
+
+
+public extension Runtime {
+    /// All classes registered with the Objective-C runtime
+    public static var allClasses: [ObjCClassDescription] {
+        var count: UInt32 = 0
+        let classList = objc_copyClassList(&count)
+        
+        var allClasses = [ObjCClassDescription]()
+        
+        for i in 0..<count {
+            guard let `class` = classList.unsafelyUnwrapped[Int(i)] else { continue }
+            allClasses.append(ObjCClassDescription(`class`))
+        }
+        
+        return allClasses
+    }
+    
+    /// Check whether a class with the given name exists
+    ///
+    /// - Parameter name: A class name
+    /// - Returns: `true` if the class exists, otherwise `false`
+    public static func classExists(_ name: String) -> Bool {
+        return objc_getClass(name.cString(using: .utf8)) != nil
+    }
+    
+    public static func getClass(_ name: String) -> NSObject.Type? {
+        return objc_getClass(name.cString(using: .utf8)) as? NSObject.Type
+    }
+}
